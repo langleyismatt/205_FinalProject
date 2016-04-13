2adab959ccda96bd9e62976c8cbbe6992842c1bf
Matthew <malangley@csumb.edu>
2016-04-13 15:37:24 -0700

    removed extra files and added picture to file picker GUI

--- filePickerGUI.py
+++ filePickerGUI.py
@@ -3,20 +3,21 @@ import tkinter as tk
 from tkinter import *
 import sys
 
-version = "Image to 8-Bit Sprite Converter!"
+title = "Image to 8-Bit Sprite Converter!"
+image = "welcome.png"
 
 var = ccbox ( "Welcome to our image to 8-bit sprite converter by Matthew Langley"
               ", Coleman Johnston, and Kailey Sarmiento!  Instructions are very simple"
               "- select an image, then watch the magic happen!                         "
-              "Are you ready?!?", title = version )
+              "Are you ready?!?", image = image, title = title )
 
 if var == 1:
-    msgbox ( msg = "Please select a file to continue...", title = version )
+    msgbox ( msg = "Please select a file to continue...", title = title )
     path = fileopenbox()
         
     
 elif var == 0:
-    msgbox ( msg = "Maybe next time!", title = version )
+    msgbox ( msg = "Maybe next time!", title = title )
     sys.exit(0)
 
 
