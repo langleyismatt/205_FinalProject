45e5113d07bfafc75f80e68d2aca61174da764ab
Matthew <malangley@csumb.edu>
2016-05-04 15:35:58 -0700

    newest GUI with option buttons

--- newGUI.py
+++ newGUI.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter.filedialog import askopenfilename
+from tkinter.colorchooser import *
 
 def close(root):
    root.destroy()
@@ -15,7 +16,7 @@ def creditss():
    text = canvas.create_text(300,100, text='', font = ("Helvetica", 32))
 
    string=("Thank you for giving our project a try!"
-            "  This project was made by the brilliant minds of Coleman Johnson, "
+            "  This project was made by the brilliant minds of Coleman Johnston, "
             "Kailey Sarmiento, and Matthew Langley.  The intent of this program is "
             "to demonstrate some of the various libraries we have learned about in "
             "to Spring 2016 semester of CST 205.")
@@ -29,9 +30,40 @@ def creditss():
       canvas.after(delay, update)
       delay += delta
 
+def getColor():
+   color = askcolor()
+   pick = 20
+   print(color[0])
+
+def bgImagePicker():
+   name = askopenfilename(title = "Custom Background Image")
+   pick = 10
+   print(name)
+   
 def filePicker():
    name = askopenfilename(title = "Choose a file")
-   return name
+   root = Tk()
+   root.title("Options")
+   root.resizable(FALSE,FALSE)
+   root.geometry("700x500")
+   
+   w = Text ( root, padx="30", pady="20", font = ("Helvetica", 12),
+             wrap="word", bg="lightblue", width="700", height="300", spacing2="7"
+              )
+
+   w.insert(INSERT, "You chose the file...\n\n" + name + "\n\nPlease "
+            "select a background color or image to continue:")
+   w.pack()
+   
+   c = Button (root, text="Select Background Color", bg="lightblue",
+               command=getColor, activebackground="#B4EEB4")
+   c.place(relx=0.15, rely=.32, anchor=CENTER)
+
+   d = Button (root, text="Select Background Image", bg="lightblue",
+               command=bgImagePicker, activebackground="#B4EEB4")
+   d.place(relx=.39, rely=.32, anchor=CENTER)
+   
+   mainloop()
 
 def controls():
    root = Tk()
@@ -40,13 +72,15 @@ def controls():
    root.geometry("300x300")
    
    w = Text ( root, padx="30", pady="60", font = ("Helvetica", 12),
-             wrap="word", bg="lightblue", width="300", height="300")
+             wrap="word", bg="lightblue", width="300", height="300", spacing2="7"
+              )
    w.insert(INSERT, "To control your character utilize the arrow keys to move around."
                      "  Press space to jump your character and click the mouse to give "
                      "him/her a good poke.  Have fun!")
    w.config(state=DISABLED)
-   t = Button ( root, text="Close Window", bg="lightblue", cursor="x_cursor", width="300",
-                command=lambda: close(root), activebackground="#B4EEB4")
+   t = Button ( root, text="Close Window", bg="lightblue", cursor="x_cursor",
+                width="300", command=lambda: close(root),
+                activebackground="#B4EEB4")
    t.pack()
    w.pack()
 
@@ -57,14 +91,15 @@ def openInstructions():
    root.geometry("300x300")
    
    w = Text ( root, padx="30", pady="60", font = ("Helvetica", 12),
-             wrap="word", bg="lightblue", width="300", height="300")
+             wrap="word", bg="lightblue", width="300", height="300", spacing2="7")
    w.insert(INSERT, "To use this program is simple.  To start, please click 'Pick-A-File'"
                      "and then select an image that you would like to process into"
                      " an 8-bit character.  From there, sit back and let the magic "
                      "happen!")
    w.config(state=DISABLED)
-   t = Button ( root, text="Close Window", bg="lightblue", cursor="x_cursor", width="300",
-                command=lambda: close(root), activebackground="#B4EEB4")
+   t = Button ( root, text="Close Window", bg="lightblue", cursor="x_cursor",
+                width="300", command=lambda: close(root),
+                activebackground="#B4EEB4")
    t.pack()
    w.pack()
 
@@ -75,36 +110,35 @@ def main():
    root.resizable(FALSE,FALSE)
    root.geometry("600x600")
 
-##   menubar = Menu(root, cursor="heart", bg="lightblue", fg="lightblue", activeforeground="lightblue", activebackground="blue")
-##   instructionsmenu = Menu(menubar, tearoff=0)
-##   instructionsmenu.add_command(label="How to use", command=openInstructions)
-##   instructionsmenu.add_separator()
-##   instructionsmenu.add_command(label="Exit", command=lambda: close(root))
-##   menubar.add_cascade(label="Instructions", menu=instructionsmenu)
-##   startmenu = Menu(menubar, tearoff=0)
-##   startmenu.add_command(label="Pick a file", command=filePicker)
-##   menubar.add_cascade(label="Start", menu=startmenu)
-##   root.config(menu=menubar)
-
-   label = Text( root, cursor="gumby", wrap="word", width="300", font = ("Helvetica", 36),
-             height="150", padx="50", pady="50", bg="lightblue", justify="CENTER")
+   label = Text( root, cursor="gumby", wrap="word", width="300",
+            font = ("Helvetica", 36),
+             height="150", padx="50", pady="50", bg="lightblue")
    label.insert(INSERT, "Welcome to our project, please select an option to continue")
    label.config(state=DISABLED)
    
-   x = Button ( root, text="How to upload a file", bg="lightblue", cursor="question_arrow", width="100",
-                height="3", command=openInstructions, activebackground="#B4EEB4")
+   x = Button ( root, text="How to upload a file", bg="lightblue",
+                cursor="question_arrow", width="100",
+                height="3", command=openInstructions,
+                activebackground="#B4EEB4")
 
-   xx = Button ( root, text="How to control your sprite", bg="lightblue", cursor="man", width="100",
-                height="3", command=controls, activebackground="#B4EEB4")
+   xx = Button ( root, text="How to control your sprite",
+                 bg="lightblue", cursor="man", width="100",
+                height="3", command=controls,
+                 activebackground="#B4EEB4")
 
-   y = Button ( root, text = "Pick-A-File", bg="lightblue", cursor="rtl_logo", width="100",
-                height="3", command=filePicker, activebackground="#B4EEB4")
+   y = Button ( root, text = "Pick-A-File", bg="lightblue",
+                cursor="rtl_logo", width="100",
+                height="3", command=filePicker,
+                activebackground="#B4EEB4")
 
-   z = Button ( root, text = "Exit", bg="lightblue", cursor="X_cursor", width="100",
-                height="3", command=lambda: close(root), activebackground="#B4EEB4")
+   z = Button ( root, text = "Exit", bg="lightblue", cursor="X_cursor",
+                width="100",
+                height="3", command=lambda: close(root),
+                activebackground="#B4EEB4")
 
-   zz = Button ( root, text = "Credits", bg="lightblue", cursor="heart", width="100",
-                height="3", command=creditss, activebackground="#B4EEB4")
+   zz = Button ( root, text = "Credits", bg="lightblue", cursor="heart",
+                 width="100", height="3", command=creditss,
+                 activebackground="#B4EEB4")
 
    frame = Frame(width=600, height=10, bg="lightblue", colormap="new")
    frame1 = Frame(width=600, height=10, bg="lightblue", colormap="new")
